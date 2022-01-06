0a3b164f288f1fe5d7f37e9e0d40e18d6860de8d
yevge <yevgen.letin@gmail.com>
2022-01-06 23:53:12 +0100

    secret key

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import secret as secrets
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qt*sf^*gjqvr*o98_n+^s@c-i&+1u*r%yh)-s@c#quyn)*=-wd'
+SECRET_KEY = secrets
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
