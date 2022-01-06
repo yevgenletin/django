c8ed99899d0976f13c95374f903977e8c514afbf
yevge <yevgen.letin@gmail.com>
2022-01-06 19:00:01 +0100

    vistas y rutas

---
+++ ProyectoAppWeb/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProyectoappwebConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ProyectoAppWeb'
---
+++ ProyectoWeb/urls.py
@@ -0,0 +1,27 @@
+"""ProyectoWeb URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from ProyectoAppWeb import views
+urlpatterns = (
+    path('admin/', admin.site.urls),
+    path('', views.home, name="Home"),
+    path('servicios', views.servicios, name="Servicios"),
+    path('tienda', views.tienda, name="Tienda"),
+    path('blog', views.blog, name="Blog"),
+    path('contacto', views.contacto, name="Contacto")
+)
